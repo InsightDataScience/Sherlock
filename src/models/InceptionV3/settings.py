a840b7720b8fced37bdbcdd048a1f468b6db31af
Runsheng Song <srs90324@gmail.com>
2018-06-13 23:32:16 -0700

    remodel inference server to accomodate different models

--- src/models/InceptionV3/settings.py
+++ src/models/InceptionV3/settings.py
@@ -1,7 +1,7 @@
 import os
 
-InceptionV3_MODEL_PATH = os.path.join("models", "InceptionV3", "InceptionV3.h5")
-BATCH_SIZE =32
+InceptionV3_MODEL_PATH = os.path.join("models", "InceptionV3")
+BATCH_SIZE = 32
 IMAGE_QUEUE = 'inceptionV3_image_queue'
 IMAGE_TYPE = 'float32'
 IMAGE_SHAPE = (1, 299, 299, 3)
