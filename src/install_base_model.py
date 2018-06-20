cd779d979ff59185fdf66e90eb4db9a34009ec2d
Runsheng Song <srs90324@gmail.com>
2018-06-20 14:50:34 -0700

    clean up died images upon starts

--- src/install_base_model.py
+++ src/install_base_model.py
@@ -1,5 +1,6 @@
 import os
 import keras
+import redis
 
 BASE_MODEL_PATH = os.path.join("app", "models", "InceptionV3", "base", "base.h5")
 TOPLESS_MODEL_PATH = os.path.join("app", "models", "InceptionV3", "topless")
@@ -26,3 +27,9 @@ else:
                                                     input_shape=(299, 299, 3))
     base_model.save(os.path.join(TOPLESS_MODEL_PATH, "topless.h5"))
     print "* Starting: Topless Model Saved!"
+    
+# clean up the died images upon start:
+# need to wait a bit for redis container to start up
+pool = redis.ConnectionPool(host='redis', port=6379, db=0)
+db = redis.Redis(connection_pool=pool)
+db.flushall()
