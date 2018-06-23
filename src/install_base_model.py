47b5e7ac4f9e0c67aed15a440e3e97714e8086ef
Runsheng Song <srs90324@gmail.com>
2018-06-22 18:16:05 -0700

    make import compatible with keras 1.2.2

--- src/install_base_model.py
+++ src/install_base_model.py
@@ -1,6 +1,6 @@
 import os
-import keras
 import redis
+from keras.applications.inception_v3 import InceptionV3
 
 BASE_MODEL_PATH = os.path.join("app", "models", "InceptionV3", "base", "base.h5")
 TOPLESS_MODEL_PATH = os.path.join("app", "models", "InceptionV3", "topless")
@@ -10,9 +10,8 @@ if os.path.exists(BASE_MODEL_PATH):
     print "* Starting: Found Base Model."
 else:
     print "* Starting: No Base Model Found. Loading..."
-    base_model = keras.applications.inception_v3.InceptionV3(include_top=True, 
-                                                    weights='imagenet',
-                                                    input_shape=(299, 299, 3))
+    base_model = InceptionV3(include_top=True, weights='imagenet',input_shape=(299, 299, 3))
+    
     base_model.save(BASE_MODEL_PATH)
     print "* Starting: Base Model Saved!"
     
@@ -22,9 +21,7 @@ if os.path.exists(TOPLESS_MODEL_PATH):
 else:
     os.makedirs(TOPLESS_MODEL_PATH)
     print "* Starting: No Topless Model Found. Loading..."
-    base_model = keras.applications.inception_v3.InceptionV3(include_top=False, 
-                                                    weights='imagenet',
-                                                    input_shape=(299, 299, 3))
+    base_model = InceptionV3(include_top=False, weights='imagenet', input_shape=(299, 299, 3))
     base_model.save(os.path.join(TOPLESS_MODEL_PATH, "topless.h5"))
     print "* Starting: Topless Model Saved!"
     
