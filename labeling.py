857f239e099cc46b878ab24bbbdb433cd85437ac
aaron <aaronh2014@gmail.com>
2019-01-24 13:59:55 -0800

    outline of process

---
+++ labeling.py
@@ -0,0 +1,28 @@
+class Image:
+    def __init__(self,fname):
+        pass
+    
+
+
+def labelingPriority(img_folder, trained_model, labeled_folder):
+
+    #order images
+    #return array of file handlers ordered by priority
+    pass
+
+def main(model_name,base_model='inceptionV3', iterations=3, slices=5,
+         bucket='insightai2019'):
+    #load the images - array of Images
+    #initialize benchmarks
+    for range(iterations):
+        #analyze the images - priority q?
+        for range(slices):
+            #update model with front of the queue
+        #run benchmarks
+
+    #display benchmarks
+        
+
+    
+
+
