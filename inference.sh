72155b300424c092e6eae075d2f18e0045532473
aaron <aaronh2014@gmail.com>
2019-01-30 11:58:22 -0800

    added model as a parameter to inference script

--- inference.sh
+++ inference.sh
@@ -4,4 +4,4 @@ curl -X POST \
   -H 'Postman-Token: eeedb319-2218-44b9-86eb-63a3a1f62e14' \
   -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
   -F image=@$1 \
-  -F model_name=base
+  -F model_name=$2
