67882463bdecbe486c181720e31e4c88161f3bab
aaron <aaronh2014@gmail.com>
2019-02-10 23:58:37 -0800

    pushing cluster based approach

--- labeling.py
+++ labeling.py
@@ -6,6 +6,12 @@ import random
 import shutil
 import pickle
 import time
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from sklearn.metrics import pairwise_distances_argmin_min
+
     
 def labelingPriority(data_unlabeled, trained_model, data_labeled=None):
     # order data 
@@ -175,6 +181,44 @@ def multiModelTrain(model_name, iterations=5):
         mn = "{}-{}".format(model_name,i)
         r = trainNewModel(mn)
 
+
+def feature_extraction(file_names,your_model):
+    feature_list = []
+    for f in file_names:
+        img = image.load_img(f,target_size=(299, 299))
+
+        x = np.expand_dims(image.img_to_array(img), axis=0)
+        x = preprocess_input(x)
+#        x = x.copy(order="C")
+
+        feature = your_model.predict(x)
+        feature_np = np.array(feature)
+        feature_list.append(feature_np.flatten())
+
+    return feature_list
+
+#    feature_list_np = np.array(feature_list)
+#    return feature_list_np
+
+
+
+def pickPoints(unlabeledFeatures, model, n,labeledFeatures=[]):
+    from sklearn.metrics import pairwise_distances_argmin_min
+    labels = []
+    if len(labeledFeatures) == 0:
+        labeledFeatures = [unlabeledFeatures[0]]
+        labels.append(0)
+        n = n - 1
+    for i in range(n):
+        indices, values = pairwise_distances_argmin_min(unlabeledFeatures,
+                                                        labeledFeatures)
+        maxOfMin = np.argmax(values)
+        labeledFeatures.append(unlabeledFeatures[maxOfMin])
+        labels.append(maxOfMin)
+        print maxOfMin, values[maxOfMin]
+    return labels 
+
+
 def cluster_label(file_names,your_model,n):
     feature_list = []
     for f in file_names:
@@ -192,14 +236,14 @@ def cluster_label(file_names,your_model,n):
     kmeans = KMeans(n_clusters=n, random_state=0).fit(feature_list_np)
     return kmeans
 
-    
+
 def main(model_name, base_model='inceptionV3', N_initial=5,
          iterations=3, labelsPerRound=5, bucket='insightai2019',
          ip_addr='http:127.0.0.1:3031'):
 #    model_name = 'tomato_potato'
 #    model_name = 'imgnetmodel'
-    model_name = 'imgnet2'
-    
+#    model_name = 'imgnet2'
+    model_name = 'HotWineBike'
     base_model = 'inceptionV3'
     N_initial = 25
     iterations = 8
@@ -211,18 +255,17 @@ def main(model_name, base_model='inceptionV3', N_initial=5,
     inference_url = ip_addr + base_model + '/predict'
     status_url = ip_addr + 'tasks/info'
     retrain_url=ip_addr + 'inceptionV3/retrain'
-
+    iv3 = InceptionV3(weights='imagenet',input_shape=(299,299,3))
+    iv3_topless = InceptionV3(include_top=False, weights='imagenet',input_shape=(299,299,3))
     # load the images - array of Images
     model_name = 'HotWineBike'
     class_names, file_names = loadDirectory('./' + model_name + '/train/')
     validate_class_names, validate_file_names = loadDirectory('./' +
                                                              model_name + '/val/')
     class_names, test_file_names = loadDirectory('./' + model_name + '/test/')
-
+    
     reserve_dict = {x : file_names[x][:iterations] for x in class_names}
     file_names   = {x : file_names[x][iterations:] for x in class_names}
-    
-    # first training pass takes N_initial of each class
 
     train_dict = magicLabel(file_names, N_initial, reserve_dict,'base')
     labeled_dict = {0: train_dict}
@@ -230,6 +273,24 @@ def main(model_name, base_model='inceptionV3', N_initial=5,
     uploadToS3(train_dict,os.path.join('models',model_name,'train'))
     uploadToS3(validate_file_names, os.path.join('models',model_name,'val'))
 
+    file_list = []
+    file_labels = []
+    for k in file_names:
+        file_list.extend(file_names[k])
+        file_labels.extend([k] * len(file_names[k]))
+    unlabeledFeatures = feature_extraction(file_list, iv3_topless)
+    r1 = pickPoints(unlabeledFeatures, iv3_topless, 20)
+    labeledFiles = [file_list[idx] for idx in r1]
+    labeledFeatures = feature_extraction(labeledFiles, iv3_topless)
+
+    r2 = pickPoints(unlabeledFeatures, iv3_topless, 20, labeledFeatures=labeledFeatures)
+    labeledFiles.extend([file_list[idx] for idx in r2])
+    labeledFeatures = feature_extraction(labeledFiles, iv3_topless)
+
+    r3 = pickPoints(unlabeledFeatures, iv3_topless, 200, labeledFeatures=labeledFeatures)
+    labeledFiles.extend([file_list[idx] for idx in r3])
+    labeledFeatures = feature_extraction(labeledFiles, iv3_topless)
+    
     r = trainNewModel(model_name)
     wait_for_training(r)
     res = {0: runInferenceOnDict(test_file_names, model_name)}
@@ -252,14 +313,9 @@ def main(model_name, base_model='inceptionV3', N_initial=5,
         saveFileName = 'r{}.pickle'.format(i+1)
         pickleResults(output_path, saveFileName, [res,labeled_dict])
             
-    # display benchmarks
-    # pickle data
-
 if __name__ == '__main__':
     main('tomato_potato')
 
 
 
 #HotCatBike = 2909.00191564s: (0.8719101123595505 training acc, 0.6842105263157895 validation acc
-queryInferenceServer(fileName, model_name='base')
-queryInferenceServer(fileName, model_name='imgnet2')
